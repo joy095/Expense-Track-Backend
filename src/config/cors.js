226d074399110976407e73299a2a589805837079
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-21 10:19:33 +0530

    update

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,14 @@
 /** @format */
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
+var whitelist = [process.env.CLIENT_URL, 'http://locaalhost:5173.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: [
